83c5ae2fd7f0d9b25cea3e429f1715eb71b0217e
Gabs Passos <gabspassos93@gmail.com>
2025-03-10 21:43:31 +0100

    implementado 3 testes login e custom commands

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,8 +1,31 @@
 describe('login', () => {
   it('Deve fazer login com sucesso', () => {
-    cy.viewport(1440, 900)
-    cy.visit('http://localhost:3000/')
-    cy.get('#email').type('gabspassos93@gmail.com')
-    cy.get('#password').type('12345')
+    cy.Start()
+
+    cy.SubmitLoginForm('papito@webdojo.com', 'katana123')
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible')
+      .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+
+  })
+
+  it('Não deve fazer login com senha errada', () => {
+    cy.Start()
+
+    cy.SubmitLoginForm('papito@webdojo.com', 'katana12345')
+
+    cy.contains('Acesso negado! Tente novamente.')
+      .should('be.visible')
+
+  })
+
+  it('Não deve fazer login com email não cadastrado', () => {
+    cy.Start()
+
+    cy.SubmitLoginForm('papitodev@webdojo.com', 'katana123')
+
+    cy.contains('Acesso negado! Tente novamente.')
+    .should('be.visible')
   })
 })
